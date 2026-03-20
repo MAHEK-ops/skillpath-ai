aff5241b05da5eb1e0d90660d4282bd50509615c
Mahek Motwani <muskanmotwani0611@gmail.com>
2026-03-21 03:32:45 +0530

    init: folder structure and page routing setup

---
+++ frontend/src/pages/LandingPage.jsx
@@ -0,0 +1,9 @@
+function LandingPage({ onNavigate }) {
+  return (
+    <div className="text-white text-4xl p-10">
+      Landing Page
+    </div>
+  )
+}
+
+export default LandingPage
---
+++ frontend/src/pages/UploadPage.jsx
@@ -0,0 +1,8 @@
+function UploadPage({ onNavigate }) {
+  return (
+    <div className="text-white text-4xl p-10">
+      Upload Page
+    </div>
+  )
+}
+export default UploadPage
